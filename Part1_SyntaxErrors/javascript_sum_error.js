7966f64f470c302e5289204988887242a597cfb8
Anushka Nanaware <ananaware42356@ucumberlands.edu>
2025-09-21 22:38:09 -0500

    Create javascript_sum_error.js

---
+++ Part1_SyntaxErrors/javascript_sum_error.js
@@ -0,0 +1,12 @@
+// JavaScript : Calculate the sum of an array with intentional errors
+function calculateSum(arr) {
+  let total = o; // <-- ERROR: letter 'o' instead of number 0
+  for (let num of arr) {
+    total += num;
+  }
+  return total;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let result = calculate Sum (numbers); // <-- ERROR: space splits identifier
+console.log("Sum in JavaScript:", result);
